1e7cef9b1b3aae0da6d00d8df6b30585e57356b0
nickwells <wellsnick11@yahoo.com>
2022-01-17 10:13:48 -0800

    commit tree 2df0e14a69a8d07399870ac6b9f8b1915f2e4ec3

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers,string){
+    return drivers.filter(
+        (kindaMatch) => kindaMatch.toLowerCase() === string.toLowerCase()
+    )}
+
+function fuzzyMatch(drivers,string){
+    return drivers.filter(
+          (firstLetter) => firstLetter.toLowerCase().indexOf(string.toLowerCase()) === 0
+)}
+
+function matchName(drivers,string){
+    return drivers.filter( 
+        (homeTown)=> homeTown.name === string)
+}
